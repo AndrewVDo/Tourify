f310a9e276a58393d24534b5cb37d23807d39bc4
Andrew Do <andrew.do1613@gmail.com>
2020-02-16 17:11:50 -0800

    successfully getting username and password to server

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(pino);
 
 app.get('/api/greeting', (req, res) => {
@@ -11,6 +12,14 @@ app.get('/api/greeting', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
+
+app.post('/login', (req, resp) => {
+  console.log(req.body.username)
+  
+  resp.send(JSON.stringify({ 
+    result: 'success' 
+  }))
+})
 
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
