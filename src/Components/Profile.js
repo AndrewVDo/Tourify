ca38cbdef51490558cae29329ca7c7abc5417d58
ata83 <ata83@sfu.ca>
2020-02-28 15:20:46 -0800

    added profileInfo to prevent multiple gets

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,6 +1,8 @@
 import React,{useState, useEffect} from "react";
+import ProfileInfo from "./ProfileInfo";
 
-const Profile =()=>{
+const Profile = (props) => {
+
     const [name, setName] = useState('');
     // probably remove later, POC
     const [password, setPassword] = useState('');
@@ -11,16 +13,24 @@ const Profile =()=>{
     const [email, setEmail] = useState('a@a.com');
 
 
-    let getresp = fetch('/getProfile')
-        .then(resp => resp.json())
-        .then(json =>{
-            console.log(json.username);
-            setName(json.username);
-            setPassword(json.password);
-        })
 
-    return (
-        <div id="profile">
+    //placeholder
+    let username_passed_in = 'asdf';
+
+    let respUserName;
+    let respPassword;
+    console.log('hi2')
+    getProfile(username_passed_in)
+        .then(resp => {
+            respUserName = resp.username;
+            respPassword = resp.password;
+            //setName(resp.username);
+            //setPassword(resp.password);
+            <ProfileInfo username = {respUserName} password = {respPassword}/>
+        });
+
+    return <ProfileInfo/>;
+    /* variables to be added to profile info
             <h1>username : {name}</h1>
             <h1>password : {password}</h1>
             <p>{role}</p>
@@ -28,10 +38,18 @@ const Profile =()=>{
             <p>User id: {userId}</p>
             <p>Date Of Birth: {birthday}</p>
             <p>Email: {email}</p>
-        </div>
-    );
+     */
 }
 
-
+var getProfile = async (username) =>{
+    return fetch(`/getProfile?username=${encodeURIComponent(username)}`, {
+        method: 'GET',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(resp => resp.json())
+}
 
 export default Profile;
