430d843332f302b1ea695193cc2955e98e174ca0
Rocky Ken <rken@sfu.ca>
2020-02-28 13:49:41 -0800

    test

---
+++ src/Components/CreateEvent.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react'
+
+const CreateEvent = () => {
+
+    return (
+        <h1>Rocky's Page</h1> 
+    )
+
+
+}
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './StyleSheets/index.css';
 import LoginPage from './Components/LoginPage/LoginPage.js';
 import * as serviceWorker from './serviceWorker';
+import CreateEvent from "./Compenents/CreateEvent.js";
 
-ReactDOM.render(<LoginPage />, document.getElementById('root'));
+ReactDOM.render(<CreateEvent />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
