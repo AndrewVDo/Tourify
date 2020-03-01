34d1292014cb3771a9992ce23c4cb7feb8e816a3
Hoi Lun Ken <rken@sfu.ca>
2020-02-29 16:16:41 -0800

    Modified: CreateEvent.js
    Added:
    Finished material UI and CSS components

--- src/Components/CreateEvent.js
+++ src/Components/CreateEvent.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import '../StyleSheets/CreateEvent.css'
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Input from '@material-ui/core/Input';
@@ -6,10 +7,11 @@ import Input from '@material-ui/core/Input';
 const CreateEvent = (props) => {
 
     return (
-        <div>
+        <div class="CreateEvent">
+            <h1 class="title">Create Event</h1>
             <form action = "" method="post">
                 <br></br>
-                <TextField id="name" label="Event Name" variant="filled" color="secondary"/>
+                <TextField id="name" label="Event Name" variant="filled"/>
                 <br></br><br></br>
 
                 <TextField
@@ -38,7 +40,7 @@ const CreateEvent = (props) => {
 
                 <br></br><br></br>
 
-                <input type="submit" value="Submit"></input> <br></br><br></br>
+                <Input type="submit" value="Create Event" variant="contained"></Input> <br></br><br></br>
             </form>
         </div>
     )
