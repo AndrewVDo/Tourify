e10ea87cecdf91048a0255ea99fd3fa4047a04b5
ata83 <ata83@sfu.ca>
2020-03-02 15:29:21 -0800

    changed postReq

--- src/Components/UpdateProfile.js
+++ src/Components/UpdateProfile.js
@@ -11,7 +11,7 @@ const UpdateProfile = () => {
 
     const handleSubmit = (event) =>{
         event.preventDefault();
-        fetch("/profileInfo",{
+        fetch("/updateProfileInfo",{
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
