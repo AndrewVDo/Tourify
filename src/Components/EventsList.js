7015a34f2881e7270c34191ec4091a9170efd4af
Samuel Chan <hyc18@sfu.ca>
2020-03-02 12:18:40 -0800

    basic list of events

--- src/Components/EventsList.js
+++ src/Components/EventsList.js
@@ -1,19 +1,50 @@
 import React, {useState, useEffect} from "react"
+import ReactDOM from 'react-dom'
+
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles';
+import { styled } from '@material-ui/core/styles';
+import { Table } from '@material-ui/core';
+
+import '../StyleSheets/EventList.css'
+
+// const myButton = styled(({ color, ...other }) => <Button {...other} />)({
+//     background: props =>
+//       props.color === 'red'
+//         ? 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
+//         : 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+//     border: 0,
+//     borderRadius: 3,
+//     boxShadow: props =>
+//       props.color === 'red'
+//         ? '0 3px 5px 2px rgba(255, 105, 135, .3)'
+//         : '0 3px 5px 2px rgba(33, 203, 243, .3)',
+//     color: 'white',
+//     height: 48,
+//     padding: '0 30px',
+//     margin: 8,
+//   });
 
 function ListElements(prop) {
     return (
         <tr>
             <td>
-                {prop.number}
+                {prop.number}       {/* link that when clicked on, prompt user to join event */}
             </td>
             <td>
-                {prop.name}
+                {prop.name}         {/* link that when clicked on, direct to event page */}
             </td>
             <td>
-                {prop.host}
+                {prop.host}         {/* link that when clicked on, direct to host's profile */}
             </td>
             <td>
                 {prop.numParticipants}
+            </td>
+            <td>
+                {prop.date}
+            </td>
+            <td>
+                <Button className="eventpages">View Event</Button>      {/* need to make GET request along with event # or any identifier */}
             </td>
         </tr>
     )
@@ -22,7 +53,7 @@ function ListElements(prop) {
 function EventsList(prop) {
     //access inouts
     //prop.username
-    console.log(prop.username)
+    // console.log(prop.username)
     //const [password, setPassword] = useState('')
 
     var events = [];
@@ -34,22 +65,43 @@ function EventsList(prop) {
         event.name = "Ride " + i;
         event.host = "Sam";
         event.numParticipants = 0;
+        event.date = "Mar " + i;
 
         events.push(event);
         numEvent++;
     }
 
-    return (
+    var button_createEvent = null;
+    var isAdmin = true;
+    if (isAdmin) {
+        button_createEvent = <Button id="createeventbutton" color="red">Create New Event</Button>
+    }
+
+    var button_viewProfile = <Button id="viewprofilebutton" color="blue">View Your Profile</Button>
+
+    const buttons = (
+        <div class="btn-group">
+            {button_viewProfile}
+            {button_createEvent}
+        </div>
+    )
+
+    const table = (
         <div className="listofevents">
-            <table border="1" id="listofevent">
+            {/* <React.Fragment> */}
+                {buttons}
+            {/* </React.Fragment> */}
+            <Table border="1" id="listofevent">
                 <tr>
-                    <th colspan="4">Ongoing Events</th>
+                    <th colspan="6">Ongoing Events</th>
                 </tr>
                 <tr>
-                    <th>Event #</th>
-                    <th>Event Name</th>
-                    <th>Host</th>
-                    <th># Participants</th>
+                    <th width="">Event #</th>
+                    <th width="">Event Name</th>
+                    <th width="">Host</th>
+                    <th width=""># Participants</th>
+                    <th width="">Event Date</th>
+                    <th width=""></th>
                 </tr>
                 {
                     events.map(element => (
@@ -58,11 +110,16 @@ function EventsList(prop) {
                             name = {element.name}
                             host = {element.host}
                             numParticipants = {element.numParticipants}
+                            date = {element.date}
                         />
                     ))
                 }
-            </table>
+            </Table>
         </div>
+    );
+
+    return (
+        table
     )
 }
 
