3fe9dca84b2681fd34bcb457bd5abe0c0af8ec8c
Andrew Do <ada62@sfu.ca>
2020-03-13 19:49:38 -0700

    Revert "Merge branch 'revert-b6957871' into 'master'"
    This reverts merge request !11

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react'
 import {BrowserRouter, Route} from 'react-router-dom';
 import CreateEvent from './Components/CreateEvent';
-import Map from "./Components/Map";
 import LoginPage from './Components/LoginPage/LoginPage'
+import EventsList from './Components/EventsList'
+import Map from "./Components/Map";
 import ProfilePage from './Components/Profile'
 import UpdateProfile from "./Components/UpdateProfile";
 
@@ -10,12 +11,12 @@ class App extends Component {
   render() {
     return (
         <BrowserRouter>
-            <Route path="/CreateEvent" component={CreateEvent}/>
-            <Route path="/profile/:userId" exact component={ProfilePage}/>
-            <Route path="/profile/:userId/edit" component = {UpdateProfile}/>
-            <Route exact path="/CreateEvent" component={CreateEvent}/>
-            <Route exact path={"/events/:eventId"} component={Map} />
-            <Route path="/" exact component={LoginPage}/>
+          <Route exact path="/create-event" component={CreateEvent}/>
+          <Route exact path="/events" component={EventsList}/>
+          <Route exact path={"/events/:eventId"} component={Map} />
+          <Route path="/profile/:userId" exact component={ProfilePage}/>
+          <Route path="/profile/:userId/edit" component = {UpdateProfile}/>
+          <Route path="/" exact component={LoginPage}/>
         </BrowserRouter>
     )
   }
