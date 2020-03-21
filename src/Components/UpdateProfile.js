1f09da5a6f90144bb16829b180480530f75289ec
Jacky Lui <jacky.lui@outlook.com>
2020-03-21 13:18:04 -0700

    Reformatting

--- src/Components/UpdateProfile.js
+++ src/Components/UpdateProfile.js
@@ -1,56 +1,54 @@
-import React, {useState} from "react";
-import {Button, MenuItem, Select, TextField} from "@material-ui/core";
-import {getNames} from 'country-list'
-import {auth} from '../firebase.js'
+import React, {useState} from 'react';
+import {Button, MenuItem, Select, TextField} from '@material-ui/core';
+import {getNames} from 'country-list';
+import {auth} from '../firebase.js';
 import {updateProfile} from '../api';
 
-import "../StyleSheets/UpdateProfile.css"
-import DateFnsUtils from "@date-io/date-fns";
-import {KeyboardDatePicker, MuiPickersUtilsProvider} from "@material-ui/pickers";
+import '../StyleSheets/UpdateProfile.css';
+import DateFnsUtils from '@date-io/date-fns';
+import {KeyboardDatePicker, MuiPickersUtilsProvider,} from '@material-ui/pickers';
 
-import {Redirect} from 'react-router-dom'
+import {Redirect} from 'react-router-dom';
 
-const nationalityList = getNames()
+const nationalityList = getNames();
 
 //TODO RECIEVE UID IN PROPS TO UPDATE, USE UID TO SPECIFY WHICH PROFILE TO GO TO
 
-const UpdateProfile = (props) => {
+const UpdateProfile = props => {
     const [name, setName] = useState();
     const [weight, setWeight] = useState();
     const [age, setAge] = useState();
     const [nationality, setNationality] = useState(nationalityList[0]);
-    const [profileRedirect, setProfileRedirect] = useState(false)
+    const [profileRedirect, setProfileRedirect] = useState(false);
     const [UId, setUId] = useState();
 
     function handleOnSubmitEvent() {
         setProfileRedirect(true);
     }
-    const handleSubmit = async (event) => {
+    const handleSubmit = async event => {
         event.preventDefault();
         await updateProfile(UId, name, weight, age, nationality);
         handleOnSubmitEvent();
-    }
+    };
 
-auth.onAuthStateChanged(user => {
-    if (user.uid.localeCompare(props.match.params.userId)!=0) {
-        console.log("hello")
-        setUId(props.match.params.userId)
-        //return (<Redirect to={`/profile/${props.match.params.userId}`}/>)
-        setProfileRedirect(true);
-    }
-    else{
-        setUId(user.uid)
-    }
-
-})
-    if(profileRedirect){
-        return (<Redirect to={`/profile/${UId}`}/>)
+    auth.onAuthStateChanged(user => {
+        if (user.uid.localeCompare(props.match.params.userId) != 0) {
+            console.log('hello');
+            setUId(props.match.params.userId);
+            //return (<Redirect to={`/profile/${props.match.params.userId}`}/>)
+            setProfileRedirect(true);
+        } else {
+            setUId(user.uid);
+        }
+    });
+    if (profileRedirect) {
+        return <Redirect to={`/profile/${UId}`} />;
     }
     return (
-        <div id='update-profile'>
+        <div id="update-profile">
             <h1>Update Profile</h1>
             <div>
-                <hr/>
+                <hr />
             </div>
             <form id="update-profile-form">
                 <table class="update-profile-table">
@@ -82,24 +80,31 @@ auth.onAuthStateChanged(user => {
                                 label="With keyboard"
                                 format="MM/dd/yyyy"
                                 value={age}
-                                InputAdornmentProps={{position: "start"}}
-                                onChange={date => setAge(date)}/>
+                                InputAdornmentProps={{ position: 'start' }}
+                                onChange={date => setAge(date)}
+                            />
                         </MuiPickersUtilsProvider>
                     </tr>
                     <tr>
                         <Select
-                            id='nationality'
+                            id="nationality"
                             defaultValue={nationalityList[0]}
-                            onChange={event => setNationality(event.target.value)}
+                            onChange={event =>
+                                setNationality(event.target.value)
+                            }
                         >
-                            {nationalityList.map(elem => <MenuItem key={elem} value={elem}>{elem}</MenuItem>)}
+                            {nationalityList.map(elem => (
+                                <MenuItem key={elem} value={elem}>
+                                    {elem}
+                                </MenuItem>
+                            ))}
                         </Select>
                     </tr>
                 </table>
             </form>
             <Button onClick={handleSubmit}>Submit</Button>
         </div>
-    )
-}
+    );
+};
 
 export default UpdateProfile;
--- src/api.js
+++ src/api.js
@@ -1,14 +1,14 @@
-const BASE_URL = ``
+const BASE_URL = ``;
 
 export const register = async (formData, dateOfBirth) => {
     try {
         let respString = await fetch(`${BASE_URL}/register`, {
             method: 'POST',
             headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
             },
-            body : JSON.stringify({
+            body: JSON.stringify({
                 alias: formData.alias,
                 dateOfBirth: dateOfBirth,
                 idToken: formData.idToken,
@@ -16,67 +16,64 @@ export const register = async (formData, dateOfBirth) => {
                 profilePicUrl: formData.profilePicUrl,
                 uid: formData.uid,
                 userType: formData['user-type'],
-                weight: formData.weight
-            })
-        })
+                weight: formData.weight,
+            }),
+        });
 
-        return await respString.json()
+        return await respString.json();
+    } catch (err) {
+        console.error('err: ', err);
     }
-    catch(err) {
-        console.error('err: ', err)
-    }
-}
+};
 
 export const login = async idToken => {
     try {
-        let respString = await fetch(``${BASE_URL}/login``, {
+        let respString = await fetch(`${BASE_URL}/login`, {
             method: 'POST',
             headers: {
-                'Accept': 'application/json',
-                'Content-type': 'application/json'
+                Accept: 'application/json',
+                'Content-type': 'application/json',
             },
             body: JSON.stringify({
-                idToken: idToken
-            })
-        })
+                idToken: idToken,
+            }),
+        });
 
-        return await respString.json()
+        return await respString.json();
+    } catch (err) {
+        console.error('err: ', err);
     }
-    catch(err) {
-        console.error('err: ', err)
-    }
-}
+};
 
-export const getAllEvents = async (uid) => {
+export const getAllEvents = async uid => {
     try {
         let respString = await fetch(`${BASE_URL}/events`, {
-            method: "POST",
+            method: 'POST',
             headers: {
-                'Accept': 'application/json',
-                'Content-type': 'application/json'
+                Accept: 'application/json',
+                'Content-type': 'application/json',
             },
             body: JSON.stringify({
-                uid: uid
-            })
-        })
-        return (await respString.json());
+                uid: uid,
+            }),
+        });
+        return await respString.json();
+    } catch (err) {
+        console.error(err);
     }
-    catch(err) {
-        console.error(err)
-    }
-}
+};
 
 export async function getProfileInfo(userId) {
     try {
         let response = await fetch(`${BASE_URL}/profile-info`, {
             method: 'POST',
             headers: {
-                'Accept': 'application/json',
-                'Content-type': 'application/json'
+                Accept: 'application/json',
+                'Content-type': 'application/json',
             },
             body: JSON.stringify({
-                uid: userId
-            })
+                uid: userId,
+            }),
         });
 
         return await response.json();
@@ -84,8 +81,8 @@ export async function getProfileInfo(userId) {
         console.log(error);
         return {
             success: false,
-            error: error
-        }
+            error: error,
+        };
     }
 }
 
@@ -94,24 +91,24 @@ export async function updateProfile(uid, name, weight, age, nationality) {
         let response = fetch(`${BASE_URL}/update-profile-info`, {
             method: 'PUT',
             headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify({
                 uid: UId,
                 name: name,
                 weight: weight,
                 age: age,
-                nationality: nationality
-            })
+                nationality: nationality,
+            }),
         });
 
         return await response.json();
-    }catch (error) {
+    } catch (error) {
         console.log(error);
         return {
             success: false,
-            error: error
-        }
+            error: error,
+        };
     }
 }
