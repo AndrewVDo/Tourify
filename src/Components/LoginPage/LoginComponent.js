005e9c7f8b58153c9a48eed852cd25eeaed14853
Andrew Do <andrew.do1613@gmail.com>
2020-03-28 19:12:51 -0700

    prettier for login page

--- src/Components/LoginPage/LoginComponent.js
+++ src/Components/LoginPage/LoginComponent.js
@@ -1,46 +1,46 @@
-import React from 'react'
-import GoogleButton from "react-google-button";
-import {signInWithGoogle} from "../../firebase.js";
-import {login} from '../../api.js'
+import React from 'react';
+import GoogleButton from 'react-google-button';
+import { signInWithGoogle } from '../../firebase.js';
+import { login } from '../../api.js';
 
-const LoginComponent = (props) => {
+const LoginComponent = props => {
     return (
-        <div className='login-app'>
-            <h1 className='title'>Tourify</h1>
-            <form className='login-form' id='form-id'>
+        <div className="login-app">
+            <h1 className="title">Tourify</h1>
+            <form className="login-form" id="form-id">
                 <GoogleButton
                     onClick={async () => {
                         try {
-                            let loginInfo = await signInWithGoogle()
-                            props.setLoginInfo(loginInfo)
-                            let response = await login(loginInfo.credential.idToken)
-                            if(!response.success) {
-                                alert(response.msg)
-                                return
-                            } 
-                            props.setShouldRedirect(true)
-                        }
-                        catch(err) {
-                            console.error('err: ', err)
+                            let loginInfo = await signInWithGoogle();
+                            props.setLoginInfo(loginInfo);
+                            let response = await login(
+                                loginInfo.credential.idToken
+                            );
+                            if (!response.success) {
+                                alert(response.msg);
+                                return;
+                            }
+                            props.setShouldRedirect(true);
+                        } catch (err) {
+                            console.error('err: ', err);
                         }
                     }}
                 />
                 <GoogleButton
-                    label='Register With Google'
+                    label="Register With Google"
                     onClick={async () => {
                         try {
-                            let loginInfo = await signInWithGoogle()
-                            props.setLoginInfo(loginInfo)
-                            props.setIsRegisterPage(true)
-                        }
-                        catch(err) {
-                            console.error('err: ', err.msg)
+                            let loginInfo = await signInWithGoogle();
+                            props.setLoginInfo(loginInfo);
+                            props.setIsRegisterPage(true);
+                        } catch (err) {
+                            console.error('err: ', err.msg);
                         }
                     }}
                 />
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default LoginComponent
+export default LoginComponent;
--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -1,32 +1,31 @@
-import React, {useState} from 'react'
-import '../../StyleSheets/LoginApp.css'
-import LoginComponent from './LoginComponent.js'
-import RegisterComponent from './RegisterComponent.js'
-import {Redirect} from 'react-router-dom'
+import React, { useState } from 'react';
+import '../../StyleSheets/LoginApp.css';
+import LoginComponent from './LoginComponent.js';
+import RegisterComponent from './RegisterComponent.js';
+import { Redirect } from 'react-router-dom';
 
 const LoginPage = () => {
-    const [isRegisterPage, setIsRegisterPage] = useState(false)
-    const [loginInfo, setLoginInfo] = useState({})
-    const [shouldRedirect, setShouldRedirect] = useState(false)
+    const [isRegisterPage, setIsRegisterPage] = useState(false);
+    const [loginInfo, setLoginInfo] = useState({});
+    const [shouldRedirect, setShouldRedirect] = useState(false);
 
-    if(shouldRedirect){
-        return(<Redirect to='/events'></Redirect>)
+    if (shouldRedirect) {
+        return <Redirect to="/events"></Redirect>;
     }
 
-    return (isRegisterPage) ? 
-    (
-        <RegisterComponent 
-            setIsRegisterPage = {setIsRegisterPage}
-            loginInfo = {loginInfo}
-            setShouldRedirect = {setShouldRedirect}
+    return isRegisterPage ? (
+        <RegisterComponent
+            setIsRegisterPage={setIsRegisterPage}
+            loginInfo={loginInfo}
+            setShouldRedirect={setShouldRedirect}
         />
     ) : (
-        <LoginComponent 
-            setIsRegisterPage = {setIsRegisterPage}
-            setLoginInfo = {setLoginInfo}
-            setShouldRedirect = {setShouldRedirect}
+        <LoginComponent
+            setIsRegisterPage={setIsRegisterPage}
+            setLoginInfo={setLoginInfo}
+            setShouldRedirect={setShouldRedirect}
         />
-    )
-}
+    );
+};
 
-export default LoginPage
+export default LoginPage;
