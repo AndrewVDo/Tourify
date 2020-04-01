354c977ab01532e39cc84d0dce1b28564cd05811
ata83 <ata83@sfu.ca>
2020-04-01 16:58:33 -0700

    prettier'd

--- src/Components/Accolades.js
+++ src/Components/Accolades.js
@@ -5,7 +5,7 @@ import {
     TableRow,
     TableCell,
     TableBody,
-    CircularProgress
+    CircularProgress,
 } from '@material-ui/core';
 import { getUserEventParticipation } from '../api';
 import { auth } from '../firebase.js';
@@ -32,20 +32,22 @@ const Accolades = props => {
         if (AccoladesList != null) {
             return AccoladesList.map(event => {
                 const { event_name } = event;
-                return <TableRow className="accolade-row">
-                    <TableCell>{event_name.concat(" Participant")}</TableCell>
-                </TableRow>;
+                return (
+                    <TableRow className="accolade-row">
+                        <TableCell>
+                            {event_name.concat(' Participant')}
+                        </TableCell>
+                    </TableRow>
+                );
             });
         }
-        return <CircularProgress id="spinner"/>;
+        return <CircularProgress id="spinner" />;
     }
     return (
         <div id="accolade">
             <Table id="accoladeTable">
                 <h1 id="award-header"> Awards: </h1>
-                <TableBody id="award-body">
-                    {AccoladeInfo()}
-                </TableBody>
+                <TableBody id="award-body">{AccoladeInfo()}</TableBody>
             </Table>
         </div>
     );
