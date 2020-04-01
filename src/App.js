3ee74c517c4e8f17a385db3bb7dd87ad0272f6ef
Andrew Do <ada62@sfu.ca>
2020-03-31 18:52:29 -0700

    sign-in/register cookies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import CreateEvent from './Components/CreateEvent';
 import LoginPage from './Components/LoginPage/LoginPage';
@@ -6,20 +6,38 @@ import EventsList from './Components/EventsList';
 import Map from './Components/Map';
 import ProfilePage from './Components/Profile';
 import UpdateProfile from './Components/UpdateProfile';
+import { CookiesProvider, useCookies } from 'react-cookie';
 
-class App extends Component {
-    render() {
+const App = () => {
+    const [cookie, setCookie, delCookie] = useCookies(['profile']);
+
+    if (!cookie.profile || cookie.profile === 'undefined') {
         return (
+            <CookiesProvider>
+                <BrowserRouter>
+                    <Route
+                        path="/"
+                        render={props => (
+                            <LoginPage {...props} setCookie={setCookie} />
+                        )}
+                    />
+                </BrowserRouter>
+            </CookiesProvider>
+        );
+    }
+
+    return (
+        <CookiesProvider>
             <BrowserRouter>
                 <Route exact path="/create-event" component={CreateEvent} />
-                <Route exact path="/events" component={EventsList} />
+                <Route exact path="/events" render={props => (<EventsList {...props} delCookie={delCookie}/>)} />
                 <Route exact path={'/events/:eventId'} component={Map} />
                 <Route path="/profile/:userId" exact component={ProfilePage} />
                 <Route path="/profile/:userId/edit" component={UpdateProfile} />
-                <Route path="/" exact component={LoginPage} />
+                <Route exact path="/" render={props => (<EventsList {...props} delCookie={delCookie}/>)} />
             </BrowserRouter>
-        );
-    }
-}
+        </CookiesProvider>
+    );
+};
 
 export default App;
